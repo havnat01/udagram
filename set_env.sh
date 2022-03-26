43794d7c7944d713b291f247bd57333cd74d523e
Minh Vu <vungocminh1692@gmail.com>
2022-03-27 03:29:59 +0700

    Container Orchestration with Kubernetes

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=minhvn4
 export POSTGRES_PASSWORD=Minh_1992
-export POSTGRES_HOST=minhvn4-project3.cgcgum2tb8iu.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_HOST=minhvn4-project3.c7gaiu8pk2vw.us-east-2.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=minhvn4-project3
-export AWS_REGION=ap-southeast-1
+export AWS_BUCKET=udagram-bucket3
+export AWS_REGION=us-east-2
 export AWS_PROFILE=default
 export JWT_SECRET=minhvn4
 export URL=http://localhost:8100
