8dabe691cada1001fa1268e3ccc7fae51745dcac
Minh Vu <vungocminh1692@gmail.com>
2022-03-26 15:32:30 +0700

    Init Project

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=minhvn4
+export POSTGRES_PASSWORD=Minh_1992
+export POSTGRES_HOST=minhvn4-project3.cgcgum2tb8iu.ap-southeast-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=minhvn4-project3
+export AWS_REGION=us-east-1
+export AWS_PROFILE=minhvn4
+export JWT_SECRET=minhvn4
+export URL=http://localhost:8100
